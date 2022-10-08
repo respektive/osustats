663fb16d369e2fdcb5ee51efdc1a162e8102e44c
respektive <liambzw@gmail.com>
2022-10-08 23:27:34 +0200

    remove logging

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ app.get("/rankings/:type", async (req, res) => {
     const type = req.params.type ?? "top50s"
     const limit = (req.query.limit && req.query.limit < 100 && req.query.limit > 0) ? req.query.limit : 50
     const offset = req.query.offset ?? 0
-    console.log("get rankings")
+
     const rankings = await getRankings(type, limit, offset)
 
     res.status(200)
@@ -19,7 +19,7 @@ app.get("/rankings/:type", async (req, res) => {
 
 app.get('/counts/:user_id', async (req, res) => {
     const user_id = req.params.user_id
-    console.log("get counts")
+
     const counts = await getCounts(user_id)
 
     res.status(200)
