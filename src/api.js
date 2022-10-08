b869c92790507265f318ad505c3dc0b5269494c2
respektive <liambzw@gmail.com>
2022-10-08 23:06:13 +0200

    logging

--- src/api.js
+++ src/api.js
@@ -10,6 +10,7 @@ app.get("/rankings/:type", async (req, res) => {
     const type = req.params.type ?? "top50s"
     const limit = req.query.limit ?? 50
     const offset = req.query.offset ?? 0
+    console.log("get rankings")
     const rankings = await getRankings(type, limit, offset)
 
     res.status(200)
@@ -18,6 +19,7 @@ app.get("/rankings/:type", async (req, res) => {
 
 app.get('/counts/:user_id', async (req, res) => {
     const user_id = req.params.user_id
+    console.log("get counts")
     const counts = await getCounts(user_id)
 
     res.status(200)
