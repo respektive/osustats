cb69d1925f9e25b88119528dcfaa2d9f93d9fedb
respektive <liambzw@gmail.com>
2022-10-08 15:07:10 +0200

    Add retry logic

--- src/fetcher.js
+++ src/fetcher.js
@@ -2,6 +2,9 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import * as mariadb from "mariadb"
 import axios from "axios"
+import axiosRetry from 'axios-retry';
+
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
 async function fetchLeaderboardsV1(skip = 0) {
     const conn = await mariadb.createConnection({
@@ -21,41 +24,44 @@ async function fetchLeaderboardsV1(skip = 0) {
     for (const [idx, beatmap_id] of beatmapIds.slice(skip).entries()) {
         beatmapsToClear.push(beatmap_id)
 
-        const response = await axios.get(`https://osu.ppy.sh/api/get_scores?k=${process.env.OSU_API_KEY}&b=${beatmap_id}&m=0&limit=100`)
-        const beatmapScores = response.data
+        try {
+            const response = await axios.get(`https://osu.ppy.sh/api/get_scores?k=${process.env.OSU_API_KEY}&b=${beatmap_id}&m=0&limit=100`)
+            const beatmapScores = response.data
+            for (const [index, score] of beatmapScores.entries()) {
+                const position = index + 1
 
-        for (const [index, score] of beatmapScores.entries()) {
-            const position = index + 1
+                scoresToInsert.push([
+                    beatmap_id,
+                    score.score_id,
+                    score.score,
+                    score.username,
+                    score.maxcombo,
+                    score.count50,
+                    score.count100,
+                    score.count300,
+                    score.countmiss,
+                    score.countkatu,
+                    score.countgeki,
+                    score.perfect,
+                    score.enabled_mods,
+                    score.date,
+                    score.pp,
+                    score.rank,
+                    score.replay_available,
+                    position,
+                    score.user_id,
+                ])
+            }
 
-            scoresToInsert.push([
-                beatmap_id,
-                score.score_id,
-                score.score,
-                score.username,
-                score.maxcombo,
-                score.count50,
-                score.count100,
-                score.count300,
-                score.countmiss,
-                score.countkatu,
-                score.countgeki,
-                score.perfect,
-                score.enabled_mods,
-                score.date,
-                score.pp,
-                score.rank,
-                score.replay_available,
-                position,
-                score.user_id,
-            ])
-        }
-
-        if (scoresToInsert.length >= 2500 || idx + 1 == beatmapIds.slice(skip).length) {
-            await conn.query("DELETE FROM scores WHERE beatmap_id IN (?)", [beatmapsToClear])
-            const res = await conn.batch("INSERT INTO scores VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", scoresToInsert)
-            console.log(new Date, `[${idx + 1}/${beatmapIds.slice(skip).length}]`, "added", res.affectedRows, "scores for beatmap_ids", beatmapsToClear)
-            scoresToInsert = []
-            beatmapsToClear = []
+            if (scoresToInsert.length >= 2500 || idx + 1 == beatmapIds.slice(skip).length) {
+                await conn.query("DELETE FROM scores WHERE beatmap_id IN (?)", [beatmapsToClear])
+                const res = await conn.batch("INSERT INTO scores VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", scoresToInsert)
+                console.log(new Date, `[${idx + 1}/${beatmapIds.slice(skip).length}]`, "added", res.affectedRows, "scores for beatmap_ids", beatmapsToClear)
+                scoresToInsert = []
+                beatmapsToClear = []
+            }
+        } catch {
+            continue
         }
     }
 
