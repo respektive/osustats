3bacf824e89ed24db6664bb2dacfe022f36bc2da
respektive <liambzw@gmail.com>
2022-10-10 03:00:51 +0200

    fix another import

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import '@fontsource/comfortaa/300.css'
