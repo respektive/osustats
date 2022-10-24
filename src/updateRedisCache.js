0ded763752d6b99dcef1786fb357e6e46de19932
respektive <liambzw@gmail.com>
2022-10-24 17:46:08 +0200

    add clear arg to redis updater

--- src/updateRedisCache.js
+++ src/updateRedisCache.js
@@ -1,3 +1,8 @@
 import { insertIntoRedis } from "./redis.js"
 
-insertIntoRedis(true)
+const args = process.argv.slice(2)
+let clear = true
+if (args[0] && args[0] == "false")
+    clear = false
+
+insertIntoRedis(clear)
