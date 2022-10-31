62342090973b6c1c0bd5ae0cc4a5e4895b68f117
respektive <liambzw@gmail.com>
2022-10-31 19:32:50 +0100

    revert

--- src/api.js
+++ src/api.js
@@ -17,7 +17,6 @@ const port = process.env.PORT
 
 app.use(express.static(path.join(__dirname, "frontend", "build")))
 app.use(logger("dev"))
-app.use(require('express-status-monitor')());
 
 app.get("/rankings/:type?", async (req, res) => {
     const type = req.params.type && TYPES.includes(req.params.type) ? req.params.type : "top50s"
