c4b115d18d6b26708addbd76cf8b556e10b4c383
respektive <liambzw@gmail.com>
2022-11-01 00:01:37 +0100

    ability to set mode for redis update

--- src/updateRedisCache.js
+++ src/updateRedisCache.js
@@ -2,16 +2,24 @@ import { insertIntoRedis } from "./redis.js"
 
 const args = process.argv.slice(2)
 let clear = true
+let mode
 if (args[0] && args[0] == "false")
     clear = false
+if (args[1])
+    mode = args[1]
 
 
 async function main() {
     console.time("Redis Update")
-    await insertIntoRedis(clear, "")
-    await insertIntoRedis(clear, "_catch")
-    await insertIntoRedis(clear, "_taiko")
-    await insertIntoRedis(clear, "_mania")
+    if (mode) {
+        if (mode == "osu") mode = ""
+        await insertIntoRedis(clear, mode)
+    } else {
+        await insertIntoRedis(clear, "")
+        await insertIntoRedis(clear, "_catch")
+        await insertIntoRedis(clear, "_taiko")
+        await insertIntoRedis(clear, "_mania")
+    }
     console.timeEnd("Redis Update")
     process.exit(0)
 }
