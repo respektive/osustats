f6d2dda3837cdd4d48c6a8b9ad9b65dbb3827bf9
respektive <liambzw@gmail.com>
2022-11-02 13:32:16 +0100

    fix if statement

--- src/fetcher.js
+++ src/fetcher.js
@@ -115,7 +115,7 @@ async function fetchLeaderboardsV1(skip = 0, mode = 0) {
 
     console.log(mode, "done.")
     await insertIntoRedis(false, modeString)
-    if (mode = 0)
+    if (parseInt(mode) === 0)
         await redis.set("last_update", new Date().toISOString())
 }
 
